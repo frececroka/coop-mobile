f62ada54efa4d2956697669ae1d7d8668522cd1b
Lorenz Gorse <lorenz.gorse@gmail.com>
2021-09-09 14:12:29 +0200

    Add widget

---
+++ app/src/main/java/de/lorenzgorse/coopmobile/AppWidgetProvider.kt
@@ -0,0 +1,35 @@
+package de.lorenzgorse.coopmobile
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import kotlinx.coroutines.runBlocking
+
+class AppWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        runBlocking {
+            val coopClientFactory = CoopModule.coopClientFactory
+            val client = coopClientFactory.get(context)
+            if (client != null) {
+                val consumption = client.getConsumption()
+                val measure = consumption[when {
+                    consumption.size >= 2 -> 1
+                    consumption.size >= 1 -> 0
+                    else -> return@runBlocking
+                }]
+                for (appWidgetId in appWidgetIds) {
+                    val views = RemoteViews(context.packageName, R.layout.widget)
+                    views.setTextViewText(R.id.amount, measure.amount.toString())
+                    views.setTextViewText(R.id.unit, measure.unit)
+                    appWidgetManager.updateAppWidget(appWidgetId, views)
+                }
+            }
+        }
+    }
+}
