a55565de457ef6e85b4256ea3913954f92e00b40
Lorenz Gorse <lorenz.gorse@gmail.com>
2022-04-09 21:06:06 +0200

    FileUploader: don't crash on big payloads

--- app/src/main/java/de/lorenzgorse/coopmobile/FileUploader.kt
+++ app/src/main/java/de/lorenzgorse/coopmobile/FileUploader.kt
@@ -4,25 +4,47 @@ import android.content.Context
 import androidx.work.*
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-import de.lorenzgorse.coopmobile.FileUploader.UploadWorker.Companion.BYTES
 import de.lorenzgorse.coopmobile.FileUploader.UploadWorker.Companion.PATH
+import de.lorenzgorse.coopmobile.FileUploader.UploadWorker.Companion.PAYLOAD_PATH
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
+import java.io.File
+import java.nio.file.Paths
+import java.time.Duration
+import java.time.Instant
+import java.util.*
 
 class FileUploader(context: Context) {
     companion object {
         val log = LoggerFactory.getLogger(FileUploader::class.java)
     }
 
+    private val cacheDir = context.filesDir.resolve("FileUploader")
     private val workManager = WorkManager.getInstance(context)
 
-    fun upload(path: String, bytes: ByteArray) {
+    init {
+        cacheDir.mkdir()
+        cacheDir.listFiles()!!
+            .filter {
+                val lastModifed = Instant.ofEpochMilli(it.lastModified())
+                val cutoff = Instant.now().minus(Duration.ofDays(1))
+                lastModifed.isBefore(cutoff)
+            }
+            .forEach { it.delete() }
+    }
+
+    fun upload(path: String, payload: ByteArray) {
+        val payloadFile = cacheDir.resolve(UUID.randomUUID().toString())
+        payloadFile.writeBytes(payload)
+
         val constraints = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.UNMETERED)
             .build()
         val workRequest = OneTimeWorkRequestBuilder<UploadWorker>()
             .setConstraints(constraints)
-            .setInputData(workDataOf(PATH to path, BYTES to bytes))
+            .setInputData(workDataOf(PATH to path, PAYLOAD_PATH to payloadFile.absolutePath))
             .build()
         workManager.enqueue(workRequest)
     }
@@ -30,19 +52,21 @@ class FileUploader(context: Context) {
     class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
         companion object {
             const val PATH = "path"
-            const val BYTES = "bytes"
+            const val PAYLOAD_PATH = "payload_path"
         }
 
         override fun doWork(): Result {
             val path = inputData.getString(PATH) ?: return Result.failure()
-            val bytes = inputData.getByteArray(BYTES) ?: return Result.failure()
-            return runBlocking { doWork(path, bytes) }
+            val payloadPath = inputData.getString(PAYLOAD_PATH) ?: return Result.failure()
+            val payloadFile = Paths.get(payloadPath).toFile()
+            return runBlocking { doWork(path, payloadFile) }
         }
 
-        private suspend fun doWork(path: String, bytes: ByteArray): Result {
+        private suspend fun doWork(path: String, payloadFile: File): Result {
+            val payload = withContext(Dispatchers.IO) { payloadFile.readBytes() }
             val storageReference = Firebase.storage.reference.child(path)
             try {
-                waitForTask(storageReference.putBytes(bytes))
+                waitForTask(storageReference.putBytes(payload))
                 return Result.success()
             } catch (e: Exception) {
                 log.error("Upload of $path failed", e)
