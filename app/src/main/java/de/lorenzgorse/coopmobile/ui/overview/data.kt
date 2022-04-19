f5fd8802b748e358726b7078cf0de4821933426e
Lorenz Gorse <lorenz.gorse@gmail.com>
2022-04-19 22:29:51 +0200

    Use getConsumptionGeneric, but ignore it's value and errors for now

--- app/src/main/java/de/lorenzgorse/coopmobile/ui/overview/data.kt
+++ app/src/main/java/de/lorenzgorse/coopmobile/ui/overview/data.kt
@@ -3,6 +3,7 @@ package de.lorenzgorse.coopmobile.ui.overview
 import android.app.Application
 import de.lorenzgorse.coopmobile.State
 import de.lorenzgorse.coopmobile.client.CoopError
+import de.lorenzgorse.coopmobile.client.Either
 import de.lorenzgorse.coopmobile.client.UnitValue
 import de.lorenzgorse.coopmobile.data.CoopViewModel
 import de.lorenzgorse.coopmobile.liftFlow
@@ -17,7 +18,8 @@ class OverviewData(app: Application) : CoopViewModel(app) {
     val state: Flow<State<Pair<List<UnitValue<Float>>, List<Pair<String, String>>>, CoopError>> =
         liftFlow(
             load { client.getConsumption() },
+            load { Either.Right(client.getConsumptionGeneric().right()) },
             load { client.getProfile() }
-        ) { cv, pv -> Pair(cv, pv) }.share()
+        ) { cv, _, pv -> Pair(cv, pv) }.share()
 
 }
