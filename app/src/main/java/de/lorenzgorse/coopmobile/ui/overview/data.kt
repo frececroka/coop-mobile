874f35ba394610d2ec29b0a54360306868bccd0b
Lorenz Gorse <lorenz.gorse@gmail.com>
2022-04-20 20:25:20 +0200

    Read remote flag to force generic consumption

--- app/src/main/java/de/lorenzgorse/coopmobile/ui/overview/data.kt
+++ app/src/main/java/de/lorenzgorse/coopmobile/ui/overview/data.kt
@@ -1,6 +1,8 @@
 package de.lorenzgorse.coopmobile.ui.overview
 
 import android.app.Application
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
 import de.lorenzgorse.coopmobile.State
 import de.lorenzgorse.coopmobile.client.CoopError
 import de.lorenzgorse.coopmobile.client.Either
@@ -15,16 +17,25 @@ import kotlinx.coroutines.flow.Flow
 @ExperimentalCoroutinesApi
 class OverviewData(app: Application) : CoopViewModel(app) {
 
+    private val forceGenericConsumption: Boolean
+        get() = Firebase.remoteConfig.getBoolean("use_generic_consumption")
+
     val state: Flow<State<Pair<List<UnitValue<Float>>, List<Pair<String, String>>>, CoopError>> =
         liftFlow(
             load { client.getConsumption() },
-            load { Either.Right(client.getConsumptionGeneric().right()) },
+            load {
+                val consumption = client.getConsumptionGeneric()
+                when {
+                    forceGenericConsumption -> consumption
+                    else -> Either.Right(consumption.right())
+                }
+            },
             load { client.getProfile() }
         ) { cv, cvg, pv ->
-            if (cv.isEmpty() && cvg != null) {
-                Pair(cvg, pv)
-            } else {
-                Pair(cv, pv)
+            val useGenericConsumption = forceGenericConsumption || cv.isEmpty()
+            when {
+                cvg != null && useGenericConsumption -> Pair(cvg, pv)
+                else -> Pair(cv, pv)
             }
         }.share()
 
