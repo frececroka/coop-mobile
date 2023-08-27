a4fa0f80ffff064f64a3a927eefea70bff3022ab
Lorenz Gorse <lorenz.gorse@gmail.com>
2023-08-27 19:03:06 +0200

    Add metadata to html shape uploads

--- app/src/main/java/de/lorenzgorse/coopmobile/FileUploader.kt
+++ app/src/main/java/de/lorenzgorse/coopmobile/FileUploader.kt
@@ -3,7 +3,12 @@ package de.lorenzgorse.coopmobile
 import android.content.Context
 import androidx.work.*
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageMetadata
 import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.ktx.storageMetadata
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import de.lorenzgorse.coopmobile.FileUploader.UploadWorker.Companion.METADATA_PATH
 import de.lorenzgorse.coopmobile.FileUploader.UploadWorker.Companion.PATH
 import de.lorenzgorse.coopmobile.FileUploader.UploadWorker.Companion.PAYLOAD_PATH
 import kotlinx.coroutines.Dispatchers
@@ -12,6 +17,7 @@ import kotlinx.coroutines.withContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.nio.charset.StandardCharsets.UTF_8
 import java.nio.file.Paths
 import java.time.Duration
 import java.time.Instant
@@ -24,6 +30,7 @@ class FileUploader(context: Context) {
 
     private val cacheDir = context.filesDir.resolve("FileUploader")
     private val workManager = WorkManager.getInstance(context)
+    private val gson = Gson()
 
     init {
         cacheDir.mkdir()
@@ -36,7 +43,10 @@ class FileUploader(context: Context) {
             .forEach { it.delete() }
     }
 
-    fun upload(path: String, payload: ByteArray) {
+    fun upload(path: String, metadata: Map<String, String>, payload: ByteArray) {
+        val metadataFile = cacheDir.resolve(UUID.randomUUID().toString())
+        metadataFile.writeBytes(gson.toJson(metadata).toByteArray(UTF_8))
+
         val payloadFile = cacheDir.resolve(UUID.randomUUID().toString())
         payloadFile.writeBytes(payload)
 
@@ -45,7 +55,11 @@ class FileUploader(context: Context) {
             .build()
         val workRequest = OneTimeWorkRequestBuilder<UploadWorker>()
             .setConstraints(constraints)
-            .setInputData(workDataOf(PATH to path, PAYLOAD_PATH to payloadFile.absolutePath))
+            .setInputData(workDataOf(
+                PATH to path,
+                METADATA_PATH to metadataFile.absolutePath,
+                PAYLOAD_PATH to payloadFile.absolutePath,
+            ))
             .build()
         workManager.enqueue(workRequest)
     }
@@ -53,25 +67,48 @@ class FileUploader(context: Context) {
     class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
         companion object {
             const val PATH = "path"
+            const val METADATA_PATH = "metadata_path"
             const val PAYLOAD_PATH = "payload_path"
         }
 
+        private val gson = Gson()
+
         override fun doWork(): Result {
             val path = inputData.getString(PATH) ?: return Result.failure()
+            val metadataPath = inputData.getString(METADATA_PATH) ?: return Result.failure()
+            val metadataFile = Paths.get(metadataPath).toFile()
             val payloadPath = inputData.getString(PAYLOAD_PATH) ?: return Result.failure()
             val payloadFile = Paths.get(payloadPath).toFile()
-            return runBlocking { doWork(path, payloadFile) }
+            return runBlocking { doWork(path, metadataFile, payloadFile) }
         }
 
-        private suspend fun doWork(path: String, payloadFile: File): Result {
+        private suspend fun doWork(path: String, metadataFile: File, payloadFile: File): Result {
+            val metadata = loadMetadata(metadataFile)
             val payload = withContext(Dispatchers.IO) { payloadFile.readBytes() }
             val storageReference = Firebase.storage.reference.child(path)
             return try {
                 waitForTask(storageReference.putBytes(payload))
+                waitForTask(storageReference.updateMetadata(metadata))
                 Result.success()
             } catch (e: Exception) {
                 log.error("Upload of $path failed", e)
                 Result.retry()
+            }
+        }
+
+        private suspend fun loadMetadata(metadataFile: File): StorageMetadata {
+            val metadataJson =
+                withContext(Dispatchers.IO) { metadataFile.readBytes().toString(UTF_8) }
+            val mapStringStringType =
+                TypeToken.getParameterized(
+                    Map::class.java,
+                    String::class.java,
+                    String::class.java
+                ).type
+            val metadata =
+                gson.fromJson<Map<String, String>>(metadataJson, mapStringStringType)
+            return storageMetadata {
+                metadata.forEach { (key, value) -> setCustomMetadata(key, value) }
             }
         }
     }
--- app/src/main/java/de/lorenzgorse/coopmobile/MonitoredHttpClient.kt
+++ app/src/main/java/de/lorenzgorse/coopmobile/MonitoredHttpClient.kt
@@ -3,6 +3,7 @@ package de.lorenzgorse.coopmobile
 import android.content.Context
 import com.google.gson.Gson
 import de.lorenzgorse.coopmobile.client.simple.HttpClient
+import de.lorenzgorse.coopmobile.ui.overview.RealFirstInstallTimeProvider
 import okhttp3.FormBody
 import okhttp3.Response
 import org.jsoup.nodes.Document
@@ -10,6 +11,8 @@ import java.net.URL
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatter.ISO_DATE
+import java.time.format.DateTimeFormatter.ISO_DATE_TIME
 import java.util.*
 
 class MonitoredHttpClient(
@@ -24,6 +27,10 @@ class MonitoredHttpClient(
     )
 
     private val fileUploader = FileUploader(context)
+    private val userProperties = UserProperties(context)
+
+    // TODO: this is ugly
+    private val firstInstallTime = Instant.ofEpochMilli(RealFirstInstallTimeProvider().get(context))
 
     override suspend fun <T> getJson(url: String, type: Class<T>, check: (Response) -> Unit): T {
         return httpClient.getJson(url, type, check)
@@ -31,17 +38,28 @@ class MonitoredHttpClient(
 
     override suspend fun getHtml(url: String, check: (Response) -> Unit): Document {
         val html = httpClient.getHtml(url, check)
-        val now = Instant.now().atZone(ZoneId.of("UTC"))
-        val date = DateTimeFormatter.ISO_DATE.format(now)
-        val dateTime = DateTimeFormatter.ISO_DATE_TIME.format(now)
+        val now = Instant.now()
+        val date = formatInstant(ISO_DATE, now)
+        val dateTime = formatInstant(ISO_DATE_TIME, now)
         val record = Record(url, dateTime, html.body().shape())
         val recordBytes = gzip(Gson().toJson(record).toByteArray())
         val userPseudoId = userPseudoId() ?: "unknown-user"
         val uuid = UUID.randomUUID()
         val path = "htmlshape/$date/$userPseudoId/$uuid.gz"
-        fileUploader.upload(path, recordBytes)
+        val metadata = buildMap {
+            put("url", url)
+            put("firstInstallTime", formatInstant(ISO_DATE_TIME, firstInstallTime))
+            val plan = userProperties.data().plan
+            if (plan != null) {
+                put("plan", plan)
+            }
+        }
+        fileUploader.upload(path, metadata, recordBytes)
         return html
     }
+
+    private fun formatInstant(formatter: DateTimeFormatter, instant: Instant): String =
+        formatter.format(instant.atZone(ZoneId.of("UTC")))
 
     override suspend fun getText(url: String, check: (Response) -> Unit): String {
         return httpClient.getText(url, check)
